5bfef09c0227e1a64c9afd41f2ddb770e8c742f1
Clizio <clizioguedes@gmail.com>
2021-10-14 11:59:18 -0300

    Criando hook e finalizando aplicação

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content, Button } from "./styles";
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <Button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </Button>
+      </Content>
+    </Container>
+  );
+}
